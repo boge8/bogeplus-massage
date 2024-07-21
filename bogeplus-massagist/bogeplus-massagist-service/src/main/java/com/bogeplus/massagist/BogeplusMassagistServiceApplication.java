80c5a3d771165186a8ebd4b5be5ab13e918ff186
zxz <1204925285@qq.com>
2024-07-22 04:26:54 +0800

    2024722技师模块init

---
+++ bogeplus-massagist/bogeplus-massagist-service/src/main/java/com/bogeplus/massagist/BogeplusMassagistServiceApplication.java
@@ -0,0 +1,18 @@
+package com.bogeplus.massagist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+/**
+ * 描述: [技师模块-启动类]
+ * 作者: 竹子
+ */
+
+@SpringBootApplication
+public class BogeplusMassagistServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BogeplusMassagistServiceApplication.class,args);
+    }
+}
