e421b187efff524896fba4de6549040444889a9a
Perterboy <truehuangjunfeng@iCloud.com>
2024-07-21 18:10:33 +0800

    activity微服务创建
    创建了activity微服务工程

---
+++ bogeplus-activity/bogeplus-activity-service/src/test/java/com/bogeplus/activity/BogeplusActivityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bogeplus.activity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BogeplusActivityServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
