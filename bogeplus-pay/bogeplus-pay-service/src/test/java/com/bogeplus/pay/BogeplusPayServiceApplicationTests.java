5f2cec18721f159123242cb276d3ef67e97c5797
itcast <hello@itcast.cn>
2024-07-21 13:22:50 +0800

    pay微服务创建

---
+++ bogeplus-pay/bogeplus-pay-service/src/test/java/com/bogeplus/pay/BogeplusPayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bogeplus.pay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BogeplusPayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
