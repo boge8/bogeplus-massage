84554291720e618f257d6df0871ae3fdbe6cfb40
WindyDante <125888129+WindyDante@users.noreply.github.com>
2024-07-24 23:03:26 +0800

    add Coupons and select CouponsList:not test

--- bogeplus-activity/bogeplus-activity-service/src/main/java/com/bogeplus/activity/controller/CouponsInfoController.java
+++ bogeplus-activity/bogeplus-activity-service/src/main/java/com/bogeplus/activity/controller/CouponsInfoController.java
@@ -4,6 +4,7 @@ import com.bogeplus.activity.dto.CouponsDto;
 import com.bogeplus.activity.entity.CouponsInfo;
 import com.bogeplus.activity.service.CouponsInfoService;
 import com.bogeplus.common.util.Result;
+import com.bogeplus.common.util.UserUtil;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,8 @@ import org.springframework.stereotype.Controller;
 
 import javax.annotation.Resource;
 import java.util.List;
+
+import static com.bogeplus.common.enums.ServiceCode.USER_NOT_EXIST;
 
 /**
  * <p>
@@ -28,23 +31,27 @@ public class CouponsInfoController {
     @Resource
     private CouponsInfoService couponsInfoService;
 
-    @ApiOperation(value = "查询优惠券",notes = "查询用户列表下的优惠券")
-    @GetMapping("/{userId}")
-    public Result listCoupons(@ApiParam(value = "用户id")@PathVariable("userId") Long userId){
-
-        List<CouponsInfo> list = couponsInfoService.listCoupons(userId);
+    @ApiOperation(value = "查询当前用户的可用优惠券",notes = "查询用户列表下的优惠券")
+    @GetMapping("")
+    public Result listCoupons(){
+        String userId = UserUtil.getId();
+        if (userId == null)
+            return Result.faild(USER_NOT_EXIST.getMsg(),USER_NOT_EXIST.getCode());
+        List<CouponsInfo> list = couponsInfoService.listCoupons(Long.valueOf(userId));
         return Result.success(list);
 
     }
 
-    @ApiOperation(value = "新增优惠券",notes = "为用户新增优惠券")
+    // 设置优惠券过期时间(生产者设置)
+
+
+    // 分发给用户优惠券(xx模块进行调用)
+
+
+    @ApiOperation(value = "新增优惠券",notes = "商户(代指优惠券生产者)新增优惠券")
     @PostMapping("/addCoupons")
     public Result addCoupons(@ApiParam(value = "商家优惠券参数实体类") @RequestBody CouponsDto couponsDto){
-
          return couponsInfoService.addCoupons(couponsDto);
-
     }
-
-    // 优惠券应该不需要手动删除和更新吧......
 
 }
--- bogeplus-activity/bogeplus-activity-service/src/main/java/com/bogeplus/activity/service/impl/CouponsInfoServiceImpl.java
+++ bogeplus-activity/bogeplus-activity-service/src/main/java/com/bogeplus/activity/service/impl/CouponsInfoServiceImpl.java
@@ -1,5 +1,6 @@
 package com.bogeplus.activity.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.bogeplus.activity.dto.CouponsDto;
 import com.bogeplus.activity.entity.CouponsInfo;
@@ -8,8 +9,11 @@ import com.bogeplus.activity.service.CouponsInfoService;
 import com.bogeplus.common.enums.ServiceCode;
 import com.bogeplus.common.util.RedisUtil;
 import com.bogeplus.common.util.Result;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.annotation.Resource;
 import java.io.IOException;
 import java.io.Serializable;
 import java.math.BigDecimal;
@@ -33,7 +37,8 @@ import static org.springframework.beans.BeanUtils.copyProperties;
 @Service
 public class CouponsInfoServiceImpl extends ServiceImpl<CouponsInfoMapper, CouponsInfo> implements CouponsInfoService {
 
-    private final String COUPONS_LIST = "COUPONS_LIST";
+    @Resource
+    private CouponsInfoMapper couponsInfoMapper;
 
     @Override
     public Result addCoupons(CouponsDto couponsDto) {
@@ -43,45 +48,26 @@ public class CouponsInfoServiceImpl extends ServiceImpl<CouponsInfoMapper, Coupo
             return Result.faild(ServiceCode.COUPONS_LESS_THAN_ZERO.getMsg(),ServiceCode.COUPONS_LESS_THAN_ZERO.getCode());
         }
 
-        // 过期时间小于今天的情况
-        LocalDateTime now = LocalDateTime.now();
-        if (couponsDto.getExpiryDate().isBefore(now)){
-            return Result.faild(ServiceCode.COUPONS_LESS_THAN_TODAY.getMsg(),ServiceCode.COUPONS_LESS_THAN_TODAY.getCode());
-        }
-        // 过期时间需要减去今天开始计算剩余可用的天数
-        Period between = Period.between(now.toLocalDate(), couponsDto.getExpiryDate().toLocalDate());
-        // 获得天数
-        int days = between.getDays();
-
-
         CouponsInfo couponsInfo = new CouponsInfo();
         copyProperties(couponsDto,couponsInfo);
-        // 拷贝后,添加对应优惠券
-        // 并在redis留存一份具体的时间,当用户使用或到时过期再删除
-        // 设计思路:新建后,将优惠券作为列表存储到redis
-        // 当用户使用时,删除redis中的数据;当过期时,当用户查询时优惠券时,再从redis查询对应的优惠券即可
-        List<Serializable> list = new ArrayList<>();
-        list.add(couponsInfo);
-        boolean isOk = RedisUtil.setListWithIndividualExpire(COUPONS_LIST + couponsDto.getUserId(), list,days);
-        if (isOk)
-            return Result.success("优惠券添加成功");
-        return Result.faild("优惠券添加失败");
+        int inserted = couponsInfoMapper.insert(couponsInfo);
+
+        return inserted > 0 ? Result.success("优惠券添加成功") : Result.faild("优惠券添加失败");
+
     }
 
     @Override
+    @Transactional
     public List<CouponsInfo> listCoupons(Long userId) {
-        List<Serializable> list = RedisUtil.getList(COUPONS_LIST + userId);
-        List<CouponsInfo> couponsInfoList = new ArrayList<>();
-        for (Serializable serializedObj : list) {
-            try {
-                // 将字符串反序列化为CouponsInfo对象
-                CouponsInfo obj = deserialize((byte[]) serializedObj);
-                couponsInfoList.add(obj);
-            } catch (Exception e) {
-                e.printStackTrace();
-                // 处理异常，例如记录日志或返回空列表
-            }
-        }
+        // 根据用户id and 过期时间进行列出,当过期时间大于此时,说明未过期,当此时大于过期时间,说明已过期,列出并删除(后续可在后台中删除,不在此处分发任务)
+        LambdaQueryWrapper<CouponsInfo> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(CouponsInfo::getUserId,userId).and(
+                t -> t.lt(CouponsInfo::getExpiryDate,LocalDateTime.now())
+        );
+        // 执行查询操作
+        List<CouponsInfo> couponsInfoList = couponsInfoMapper.selectList(queryWrapper);
+
+        // 遍历过期的优惠券列表并进行删除(后续可优化为消息队列形式)
         return couponsInfoList;
     }
 }
