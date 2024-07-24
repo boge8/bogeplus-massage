1dfeeccd2e8ac4c88f5e7492dc5cc71fbfeea14c
hanbinXiao <warmzara@outlook.com>
2024-07-24 21:13:23 +0800

    改错

--- bogeplus-massagist/bogeplus-massagist-service/src/main/java/com/bogeplus/massagist/BogeplusMassagistServiceApplication.java
+++ bogeplus-massagist/bogeplus-massagist-service/src/main/java/com/bogeplus/massagist/BogeplusMassagistServiceApplication.java
@@ -1,7 +1,9 @@
 package com.bogeplus.massagist;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.ApplicationContext;
 
 /**
@@ -10,8 +12,9 @@ import org.springframework.context.ApplicationContext;
  */
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@MapperScan("com.bogeplus.massagist.mapper")
 public class BogeplusMassagistServiceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(BogeplusMassagistServiceApplication.class,args);
     }
