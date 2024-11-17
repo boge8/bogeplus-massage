1cf8b81e0ba519713ebcb7b2405f3a70890b3dfb
Bin <warmzara@outlook.com>
2024-11-18 00:16:54 +0800

    我删了东西，测试打印

--- bogeplus-massagist/bogeplus-massagist-service/src/main/java/com/bogeplus/massagist/service/impl/MassagistInfoItemServiceImpl.java
+++ bogeplus-massagist/bogeplus-massagist-service/src/main/java/com/bogeplus/massagist/service/impl/MassagistInfoItemServiceImpl.java
@@ -145,8 +145,6 @@ public class MassagistInfoItemServiceImpl extends ServiceImpl<MassagistInfoItemM
         } else {
             queryWrapper.eq(MassagistInfoItem::getItemId, objId)
                     .in(MassagistInfoItem::getMasseurId, objIdList);
-            System.out.println("我在这里加了东西");
-            System.out.println("我在这里加了东西");
         }
         return list(queryWrapper);
     }
